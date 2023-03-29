a32e77bc74151d02c3ba1db8b6c229cfea246c2d
mFurkanHiz <mf.wissen@gmail.com>
2023-03-29 17:54:32 +0300

    First Commit

---
+++ DbFirst/Data/WissenContext.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Teu.Canberks;
+
+public partial class WissenContext : DbContext
+{
+    public WissenContext()
+    {
+    }
+
+    public WissenContext(DbContextOptions<WissenContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-E30TBPJ;Database=NORTHWND;Trusted_Connection=True;TrustServerCertificate=Yes");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.UseCollation("SQL_Latin1_General_CP1_CI_AS");
+
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasIndex(e => e.City, "City");
+
+            entity.HasIndex(e => e.CompanyName, "CompanyName");
+
+            entity.HasIndex(e => e.PostalCode, "PostalCode");
+
+            entity.HasIndex(e => e.Region, "Region");
+
+            entity.Property(e => e.CustomerId)
+                .HasMaxLength(5)
+                .IsFixedLength()
+                .HasColumnName("CustomerID");
+            entity.Property(e => e.Address).HasMaxLength(60);
+            entity.Property(e => e.City).HasMaxLength(15);
+            entity.Property(e => e.CompanyName).HasMaxLength(40);
+            entity.Property(e => e.ContactName).HasMaxLength(30);
+            entity.Property(e => e.ContactTitle).HasMaxLength(30);
+            entity.Property(e => e.Country).HasMaxLength(15);
+            entity.Property(e => e.Fax).HasMaxLength(24);
+            entity.Property(e => e.Phone).HasMaxLength(24);
+            entity.Property(e => e.PostalCode).HasMaxLength(10);
+            entity.Property(e => e.Region).HasMaxLength(15);
+        });
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.ToTable(tb =>
+                {
+                    tb.HasTrigger("AddToLog");
+                    tb.HasTrigger("KayitIslemleriTrigger");
+                });
+
+            entity.HasIndex(e => e.LastName, "LastName");
+
+            entity.HasIndex(e => e.PostalCode, "PostalCode");
+
+            entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+            entity.Property(e => e.Address).HasMaxLength(60);
+            entity.Property(e => e.BirthDate).HasColumnType("datetime");
+            entity.Property(e => e.City).HasMaxLength(15);
+            entity.Property(e => e.Country).HasMaxLength(15);
+            entity.Property(e => e.Extension).HasMaxLength(4);
+            entity.Property(e => e.FirstName).HasMaxLength(10);
+            entity.Property(e => e.HireDate).HasColumnType("datetime");
+            entity.Property(e => e.HomePhone).HasMaxLength(24);
+            entity.Property(e => e.LastName).HasMaxLength(20);
+            entity.Property(e => e.Notes).HasColumnType("ntext");
+            entity.Property(e => e.Photo).HasColumnType("image");
+            entity.Property(e => e.PhotoPath).HasMaxLength(255);
+            entity.Property(e => e.PostalCode).HasMaxLength(10);
+            entity.Property(e => e.Region).HasMaxLength(15);
+            entity.Property(e => e.Title).HasMaxLength(30);
+            entity.Property(e => e.TitleOfCourtesy).HasMaxLength(25);
+
+            entity.HasOne(d => d.ReportsToNavigation).WithMany(p => p.InverseReportsToNavigation)
+                .HasForeignKey(d => d.ReportsTo)
+                .HasConstraintName("FK_Employees_Employees");
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasIndex(e => e.CustomerId, "CustomerID");
+
+            entity.HasIndex(e => e.CustomerId, "CustomersOrders");
+
+            entity.HasIndex(e => e.EmployeeId, "EmployeeID");
+
+            entity.HasIndex(e => e.EmployeeId, "EmployeesOrders");
+
+            entity.HasIndex(e => e.OrderDate, "OrderDate");
+
+            entity.HasIndex(e => e.ShipPostalCode, "ShipPostalCode");
+
+            entity.HasIndex(e => e.ShippedDate, "ShippedDate");
+
+            entity.HasIndex(e => e.ShipVia, "ShippersOrders");
+
+            entity.Property(e => e.OrderId).HasColumnName("OrderID");
+            entity.Property(e => e.CustomerId)
+                .HasMaxLength(5)
+                .IsFixedLength()
+                .HasColumnName("CustomerID");
+            entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+            entity.Property(e => e.Freight)
+                .HasDefaultValueSql("(0)")
+                .HasColumnType("money");
+            entity.Property(e => e.OrderDate).HasColumnType("datetime");
+            entity.Property(e => e.RequiredDate).HasColumnType("datetime");
+            entity.Property(e => e.ShipAddress).HasMaxLength(60);
+            entity.Property(e => e.ShipCity).HasMaxLength(15);
+            entity.Property(e => e.ShipCountry).HasMaxLength(15);
+            entity.Property(e => e.ShipName).HasMaxLength(40);
+            entity.Property(e => e.ShipPostalCode).HasMaxLength(10);
+            entity.Property(e => e.ShipRegion).HasMaxLength(15);
+            entity.Property(e => e.ShippedDate).HasColumnType("datetime");
+
+            entity.HasOne(d => d.Customer).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.CustomerId)
+                .HasConstraintName("FK_Orders_Customers");
+
+            entity.HasOne(d => d.Employee).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.EmployeeId)
+                .HasConstraintName("FK_Orders_Employees");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.ToTable(tb =>
+                {
+                    tb.HasTrigger("Add5");
+                    tb.HasTrigger("Subtract5");
+                });
+
+            entity.HasIndex(e => e.CategoryId, "CategoriesProducts");
+
+            entity.HasIndex(e => e.CategoryId, "CategoryID");
+
+            entity.HasIndex(e => e.ProductName, "ProductName");
+
+            entity.HasIndex(e => e.SupplierId, "SupplierID");
+
+            entity.HasIndex(e => e.SupplierId, "SuppliersProducts");
+
+            entity.Property(e => e.ProductId).HasColumnName("ProductID");
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.ProductName).HasMaxLength(40);
+            entity.Property(e => e.QuantityPerUnit).HasMaxLength(20);
+            entity.Property(e => e.ReorderLevel).HasDefaultValueSql("(0)");
+            entity.Property(e => e.SupplierId).HasColumnName("SupplierID");
+            entity.Property(e => e.UnitPrice)
+                .HasDefaultValueSql("(0)")
+                .HasColumnType("money");
+            entity.Property(e => e.UnitsInStock).HasDefaultValueSql("(0)");
+            entity.Property(e => e.UnitsOnOrder).HasDefaultValueSql("(0)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
