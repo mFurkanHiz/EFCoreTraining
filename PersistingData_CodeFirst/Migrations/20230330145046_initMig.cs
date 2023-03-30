cd3e0f197045636f2e4563db325e8718c25ac9a8
mFurkanHiz <mf.wissen@gmail.com>
2023-03-30 18:13:48 +0300

    Advancing

---
+++ PersistingData_CodeFirst/Migrations/20230330145046_initMig.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersistingData_CodeFirst.Migrations
+{
+    /// <inheritdoc />
+    public partial class initMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    KitapAdi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fiyat = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
---
+++ PersistingData_CodeFirst/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine();
+
+#region Veri Ekleme
+CodeFirstKitaplikDbContext context = new();
+//Book book = new();
+Book book = new()
+{
+    KitapAdi = "Muhittin",
+    Fiyat = 60
+};
+
+context.Add(book);
+
+//context.SaveChanges();
+await context.AddAsync(); // aynı işlem
+await context.SaveChangesAsync(); // aynısı ama daha hızlı
+
+
+// SaveChanges: insert update delete sorgularını oluşturup bir transaction eşliğinde vt'na gönderip execute eden fonksiyondur. Eğer ki sorgulardan herangi herhangi biri başarısız olursa tüm işlemler geri alınacaktır. bu işleme rollback denir [mülekatlarda sorulur]
+
+#endregion
+
+#region EFCore verinin ekleneceğini nereden anlıyor
+Book book2 = new()
+{
+    KitapAdi = "Muhittin2",
+    Fiyat = 60
+};
+
+Console.WriteLine(context.Entry(book2).State);
+
+await context.AddAsync(book2);
+
+Console.WriteLine(context.Entry(book2).State);
+
+await context.SaveChangesAsync();
+
+Console.WriteLine(context.Entry(book2).State);
+
+#endregion
+
+
+
+public class CodeFirstKitaplikDbContext : DbContext
+{
+    public DbSet<Book> Books { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=DESKTOP-E30TBPJ;Database=CodeFirstKitaplikDb;Trusted_Connection=True;TrustServerCertificate=Yes");
+    }
+}
+
+public class Book
+{
+    public int Id { get; set; }
+    public string KitapAdi { get; set; }
+    public double Fiyat { get; set; }
+}
