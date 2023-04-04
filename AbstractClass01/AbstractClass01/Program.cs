5b23ce810082338a60e7db9cc7b755d625a16bbc
mFurkanHiz <mf.wissen@gmail.com>
2023-04-04 18:02:41 +0300

    asd
    asdasdasd

---
+++ AbstractClass01/AbstractClass01/Program.cs
@@ -0,0 +1,130 @@
+﻿namespace AbstractClass01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            /*
+             * Öyle bir sınıf düşünelim ki bu sınıftan bir instance almak mümkün olmasın. Bu class tpine abstract (soyut) class denmektedir. Soyut sınıflar kendi başlarına kullanılmamaktadır. Bunun yerine bu sınıflar türetmiş oldukları sınıflar tarafından uygulanması gereken bir dizi soyut yöntemler veya özellikler tanımlarlar.
+             * 
+             * Genel kullanımları bir arayüz (interface) oluşturulmak istendiği zaman, fakat bu arayüdeki bir takım yöntemlerin uyulanmasını zorunlu tutmak istediğimiz zaman kullanmaktayız. 
+             * 
+             * Abstrat classlar ile normal classlar arasındaki farkla normal classlarda metot bildirimi sadece imzasıyla yapılmazken abstract classlarda bu yapılabilir. zaten temel amaç da budur. Abstract classlardan türhyen sınıflarda metotlar zaten imza olarak göösterilebilmektedir.
+             * 
+             * Normal sınıflarda newleme işlemi yapılabilirken abstract classlarda newleme işlemi gerçekleşrtirilemez.
+             * 
+             * Abstract metotlar sadece abstract sınıflar içerisinde kullanılabilen metotlardır. Mirasçı olan sınıflarda override edilmek zorundadırlar.
+             * 
+             * Abstract metotlar sadece tanımlanır, herhangi bir işlemi yerine getirmez. yapacakları işlemler override edildikleri sınıfta kodlanmalıdır.
+             * 
+             * Abstract elemanlar private OLAMAZ.
+             */
+
+            //Mobilya m1 = new Mobilya(); // HATA
+            // Çünkü abstract classlar newlenemez
+            // abstract classlar sadece kalıtım vermek bilgi aktarmak için kullanılır
+            Kanepe h1 = new Kanepe();
+            h1.renk = "Siyah";
+            h1.kumas = "Deri";
+            masa m1 = new masa();
+            m1.renk = "Ahşap Rengi";
+            m1.malzeme = "Ahşap";
+
+            Console.WriteLine("----------------------");
+            h1.Tanimla();
+            Console.WriteLine("----------------------");
+            m1.Tanimla();
+            Console.WriteLine("----------------------");
+
+            Dog dog1 = new Dog();
+            Dog dog2 = new Dog();
+            Cat cat1 = new Cat();
+            Cat cat2 = new Cat();
+
+            dog1.name = "asd";
+            dog2.name = "qwe";
+            cat1.name = "zxc";
+            cat2.name = "123";
+
+            dog1.eat("meat");
+            cat2.move();
+            dog2.speak();
+            cat1.eat("cat food");
+            dog1.move();
+
+
+        }
+    }
+
+    abstract class Mobilya
+    {
+        public string renk;
+        abstract public void Tanimla();
+    }
+
+    class Kanepe : Mobilya
+    {
+        public string kumas;
+        // kalıtım aldığın yerden otomatik olarak çağırmaya implement etmek denir (implementation)
+        public override void Tanimla()
+        {
+            Console.WriteLine($"Kanepenin Özellikleri: \nRenk: {renk},\nKumaş: {kumas}");
+        }
+    }
+
+    class masa : Mobilya
+    {
+        public string malzeme;
+        public override void Tanimla()
+        {
+            Console.WriteLine($"Masanın Özellikleri: \nRenk: {renk},\nMalzeme: {malzeme}");
+        }
+    }
+
+    abstract class Animal
+    {
+        public string name;
+        public int age;
+        abstract public void move();
+        abstract public void eat(string food);
+        abstract public void speak();
+    }
+
+    class Cat  : Animal
+    {
+        public override void eat(string food)
+        {
+            Console.WriteLine($"{name} eats {food}");
+        }
+
+        public override void move()
+        {
+            Console.WriteLine($"{name} moves");
+        }
+
+        public override void speak()
+        {
+            Console.WriteLine($"{name} Mouws");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void eat(string food)
+        {
+            Console.WriteLine($"{name} eats {food}");
+        }
+
+        public override void move()
+        {
+            Console.WriteLine($"{name} runs");
+        }
+
+        public override void speak()
+        {
+            Console.WriteLine($"{name} Barks");
+        }
+    }
+
+}
---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            /*
+             * Diğer sınıflara şablon oluşturmak yol göstermek amacıyla implementasyon kullanarak kendisindeki alanları doldurulması zorunlu hale getiren bir yapıdır. Interfaceler bir class değildir. Kendilerine özgü bir yapıdır.
+             * 
+             * Sınıflarda birden fazla interface implemente edilebilir. içinde neler bulunur. İnterface içindeki bağlı tüm yapılar publictir. interfacelerde neler bulunur? proplar e metotlar bulunur.
+             * 
+             * Tanımlaması yapılan memberlar interface içerisinde uygulanmazlar. Fakat kendisinden implemente edilen classlar kendi içlerinde bu imzaları bodylerine oluşturabilirler. Genelde gösterimi baına I harfi eklenerek olur (zorunlu değil ama öyle kullanılır) 
+             * 
+             * Abstract classlar interfaceler aslında benzer amaçla kullanılmaktadırlar. iki yapı da sınıfın davranış biçimine yön verir ve benzer nitelikler barındırır. 
+             * 
+                Örneğin;
+             * Soyut sınıflar hem somut(concrete) üyeler barındırır hem de soyut üyeler barındırır. soyut üyeler alt sınıflarda ezilerek kullanılır. Soyut sınıflarda kalıtım kullanılır ve alt sınıflarda newleme işlemi yaptırılır. Fakat soyut metotlar alt sınıflarda uygulanmak zorundadırlar.
+             * 
+                interfaceler ve soyut sınıflar arasındaki farklar;
+             * soyut sınıflar soyut üyeler de içerebilirken interfaceler sadece tanım içerirler.
+             * Soyut sınıflar , barındırdıkları memberların uygulanması, gerçekleşmesi için kalıtım kullanılır ve kalıtımla alt sınıflar oluşturulur fakat interfaceler birden fazla interface uygulamasına olanak tanır.
+             * Biri classtır diğeri değildir.
+             * soyut sınıflarda varsayılan olarak erişim belirtici protected tır. interfacelerde default olarak public gelir.
+             * 
+             */
+
+            Audi a = new Audi();
+            Mercedes m = new Mercedes();
+
+            a.Model = "Sedan";
+            a.Yil = 2022;
+            m.Model = "SUV";
+            m.Yil = 2021;
+            a.Tur = "asd";
+
+            a.HareketEt();
+            m.HareketEt();
+        }
+    }
+
+    public interface ITasit
+    {
+        public string Tur { get; set; }
+    }
+
+    public interface IVehicle
+    {
+        string Model { get; set; }
+        public int Yil { get; set; }
+
+        void HareketEt();
+    }
+
+    public class Audi : IVehicle, ITasit
+    {
+        public string Model { get; set; }
+        public int Yil { get; set; }
+        public string Tur { get; set; }
+
+        public void HareketEt()
+        {
+            Console.WriteLine("Audi hareket etti.");
+        }
+    }
+    public class Mercedes : IVehicle
+    {
+        public string Model { get; set; }
+        public int Yil { get; set; }
+
+        public void HareketEt()
+        {
+            Console.WriteLine("Mercedes hareket etti.");
+        }
+    }
+}
